7a90ba1b21e21cf85493d6fafbd2f4346038f6e0
Gabriel Correia <gabriel_correia@estudante.sc.senai.br>
2023-11-06 14:52:49 -0300

    first

---
+++ src/interfaces/Restaurante.ts
@@ -0,0 +1,8 @@
+export default interface Restaurante{
+    idrestaurante: number;
+    nomeEstabelecimento : string;
+    responsavel: string;
+    cnpj: string;
+    contato: string;
+    especialidade:string;
+}
